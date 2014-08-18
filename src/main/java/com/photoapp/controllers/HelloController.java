f340da06fd561717105cf4bd1a41634c51928ff4
Francisco Martin <franjmartin21@gmail.com>
2014-08-18 09:50:02 -0500

    Adding log4j2

--- src/main/java/com/photoapp/controllers/HelloController.java
+++ src/main/java/com/photoapp/controllers/HelloController.java
@@ -1,23 +1,28 @@
 package com.photoapp.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by fran on 17/08/14.
  */
 @Controller
 public class HelloController {
+	private final Logger logger = LogManager.getLogger(HelloController.class.getName());
 
     @RequestMapping("/hello")
     public ModelAndView sayHelloToOpenShift(){
+    	logger.info("sayHelloToOpenShift --> Start");
         Map<String, Object> templateModel = new HashMap<String, Object>();
         templateModel.put("firstname", "Francisco");
         templateModel.put("lastname", "Martin");
+        logger.info("sayHelloToOpenShift --> End");
         return new ModelAndView("page/hello2", templateModel);
     }
 
